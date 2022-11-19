87dddea4fbc817bae7b8fe3f3ecfdcd482c33fb1
skillsoft-demo <41397191+skillsoft-demo@users.noreply.github.com>
2022-11-18 21:34:49 -0500

    Create create_artifacts.py

---
+++ create_artifacts.py
@@ -0,0 +1,8 @@
+with open('artifact1.txt', 'w') as f:
+        print('test1', file=f)
+
+with open('artifact2.txt', 'w') as f:
+        print('test', file=f)
+
+with open('artifact3.txt', 'w') as f:
+        print('test', file=f)
