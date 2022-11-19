485fd8607cfc423181d8aaa1ea5fd06cb33eebc2
skillsoft-demo <41397191+skillsoft-demo@users.noreply.github.com>
2022-11-19 13:08:13 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import os
 text_filenames = [i for i in os.listdir() if i.endswith('.txt')]
 
 for filename in text_filenames:
-	with open(filename) as f:
-		assert 'test' in f.read()
-		print('FILE {} IS OK'.format(filename))
+    with open(filename) as f:
+        data = f.read()
+        assert 'begin' in data
+        assert 'end' in data
+        print('FILE {} IS COMPLETE'.format(filename))
